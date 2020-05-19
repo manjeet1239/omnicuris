3a1728d0e13103386e6990a0c8952db375944f93
manjeet1239 <manjeetkr154@gmail.com>
2020-05-19 13:27:06 +0530

    update repo

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './pages/HomePage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import '~video-react/dist/video-react.css'; // import css
+import VideoPlayer from './components/VideoPlayer'
+
+import Description from './components/Description';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+      {/* <Description />
+      <Card /> */}
+    </div>
+  );
+}
+
+export default App;
